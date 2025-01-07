2e77cea28136a426992f1bb65fc102701f98ffea
Thibaut Sardan <github@thib.top>
2025-01-07 16:30:27 +0000

    colors and titles

--- src/Content.tsx
+++ src/Content.tsx
@@ -21,29 +21,37 @@ export const Content = () => {
   }
 
   return (
-    <div className="m-4 flex flex-col items-center justify-center gap-4">
-      <Button size="lg" onClick={() => handleConnect('metamask')}>
-        <span className="shrink grow basis-0 text-left text-base leading-normal">
-          MetaMask
-        </span>
-        <img className="size-[35px] rounded-lg" src={metamaskIcon} />
-      </Button>
-      <Button size="lg" onClick={() => handleConnect('phantom')}>
-        <span className="shrink grow basis-0 text-left text-base leading-normal">
-          Phantom
-        </span>
-        <img className="size-[35px] rounded-lg" src={phantomIcon} />
-      </Button>
-      <Button
-        size="lg"
-        onClick={() => {
-          open().catch(console.error)
-        }}
-      >
-        <span className="shrink grow basis-0 text-left text-base leading-normal">
-          Other
-        </span>
-      </Button>
+    <div>
+      <div className="my-12 text-center ">
+        <div className="text-2xl font-normal leading-loose">
+          Welcome to Sprinter
+        </div>
+        <div className="text-secondary">Get started by selecting a wallet:</div>
+      </div>
+      <div className="m-4 flex flex-col items-center justify-center gap-4">
+        <Button size="lg" onClick={() => handleConnect('metamask')}>
+          <span className="shrink grow basis-0 text-left text-base leading-normal">
+            MetaMask
+          </span>
+          <img className="size-[35px] rounded-lg" src={metamaskIcon} />
+        </Button>
+        <Button size="lg" onClick={() => handleConnect('phantom')}>
+          <span className="shrink grow basis-0 text-left text-base leading-normal">
+            Phantom
+          </span>
+          <img className="size-[35px] rounded-lg" src={phantomIcon} />
+        </Button>
+        <Button
+          size="lg"
+          onClick={() => {
+            open().catch(console.error)
+          }}
+        >
+          <span className="shrink grow basis-0 text-left text-base leading-normal">
+            Other
+          </span>
+        </Button>
+      </div>
     </div>
   )
 }
